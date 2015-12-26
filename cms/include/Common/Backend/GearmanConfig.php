7109efd52ac8dff412efc19af127221252611465
Jose F. DSilva <jose.dsilva@bombayworks.se>
2015-12-26 19:57:26 +0530

    add config backend module.

---
+++ cms/include/Common/Backend/GearmanConfig.php
@@ -0,0 +1,73 @@
+<?php
+
+class Common_Backend_GearmanConfig extends Curry_Backend
+{
+    public static function getGroup()
+    {
+        return 'System';
+    }
+    
+    public function showMain()
+    {
+        $form = new Curry_Form(array(
+            'url' => url('', $_GET),
+            'method' => 'post',
+            'elements' => array(
+                'token' => array('text', array(
+                    'label' => 'Token',
+                    'description' => 'To execute the listener from the browser, a hash is required.',
+                    'value' => Curry_Core::$config->modules->contrib->CurryGearman->token,
+                    'required' => true,
+                    'placeholder' => 'Enter a random string',
+                )),
+                'save' => array('submit', array('label' => 'Save')),
+            ),
+        ));
+        
+        if (isPost() && $form->isValid($_POST)) {
+            $values = $form->getValues(true);
+            $this->saveConfig($values);
+            return;
+        }
+        
+        $webWorkerUrl = url(Curry_Core::$config->curry->baseUrl.'gearman_listener.php/', array('hash' => Common_Gearman_Listener::getHash()))->getAbsolute();
+        $html =<<<HTML
+$form
+<p><a href="{$webWorkerUrl}" target="_blank">Click here to execute Gearman web worker.</a></p>
+HTML;
+        
+        $this->addMainContent($html);
+    }
+    
+    protected function saveConfig(array $values)
+    {
+        $config = new Zend_Config(require(Curry_Core::$config->curry->configPath), true);
+        $config->modules = array(
+            'contrib' => array(
+                'CurryGearman' => array(
+                    'token' => $values['token'],
+                ),
+            ),
+        );
+        
+        try {
+            $writer = new Zend_Config_Writer_Array();
+            $writer->write(Curry_Core::$config->curry->configPath, $config);
+            $this->flushConfigCache();
+            $this->addMessage("Settings saved.", self::MSG_SUCCESS);
+        } catch (Exception $e) {
+            $this->addMessage($e->getMessage(), self::MSG_ERROR);
+        }
+    }
+    
+    protected function flushConfigCache()
+    {
+        if (extension_loaded('apc')) {
+            if (function_exists('apc_delete_file')) {
+                @apc_delete_file(Curry_Core::$config->curry->configPath);
+            } else {
+                @apc_clear_cache();
+            }
+        }
+    }
+}
